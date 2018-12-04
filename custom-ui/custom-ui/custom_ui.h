6d1929108cea993e4c95390e9e238d8e8a3d40d1
Natnicha Natnichakunwara <natnicha.n@23perspective.com>
2018-12-04 12:03:06 +0700

    [ add ] [ complete ] initial project

---
+++ custom-ui/custom-ui/custom_ui.h
@@ -0,0 +1,19 @@
+//
+//  custom_ui.h
+//  custom-ui
+//
+//  Created by Natnicha on 4/12/18.
+//  Copyright © 2018 Natnicha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for custom_ui.
+FOUNDATION_EXPORT double custom_uiVersionNumber;
+
+//! Project version string for custom_ui.
+FOUNDATION_EXPORT const unsigned char custom_uiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <custom_ui/PublicHeader.h>
+
+
